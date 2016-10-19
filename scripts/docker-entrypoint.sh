3f2f495f4568a3a24eff698ae2c3902cf0c3b1df
jderuere <deruere.julien@gmail.com>
2016-10-19 13:45:36 -0400

    - Fix: Pass file name as an argument to the Gurobi interactive shell

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -13,12 +13,12 @@ fi
 license=/home/gurobi/gurobi.lic
 if [ -f $license ]; then
     echo "Skipping license creation"
-    gurobi.sh
+    gurobi.sh $1
 else
     echo "Configure license $GUROBI_LICENSE"
 
     grbgetkey $GUROBI_LICENSE
     expect "Assign license key to user 'gurobi' on this machine? \[Y/n\]"
 	send "Y"
-    gurobi.sh
+    gurobi.sh $1
 fi
