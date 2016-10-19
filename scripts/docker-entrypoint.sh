22ad3a5f35eb9c9806bc1978fba481c1e09542cc
jderuere <deruere.julien@gmail.com>
2016-10-19 10:02:46 -0400

    Init commit

---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+    set -- gurobi "$@"
+fi
+
+if [[ "$VERBOSE" = "yes" ]]; then
+    set -x
+fi
+
+license=/home/gurobi/gurobi.lic
+if [ -f $license ]; then
+    echo "Skipping license creation"
+    gurobi.sh
+else
+    echo "Configure license $GUROBI_LICENSE"
+
+    grbgetkey $GUROBI_LICENSE
+    expect "Assign license key to user 'gurobi' on this machine? \[Y/n\]"
+	send "Y"
+    gurobi.sh
+fi
